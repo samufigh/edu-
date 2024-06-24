cc12a41d2f0eb28fd1fccb1e13bb490bcee0fe43
samufigh <samufigh21@gmail.com>
2024-06-24 20:43:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pagina-de-amor
+
+# Este es un proyecto en el cual podras enviarle a alguien para que sea tu pareja si o si
+
+preview: https://stalker-amor.netlify.app
+
+# edu-
---
+++ script.js
@@ -0,0 +1,37 @@
+
+function displace() {
+    this.style.marginLeft == "400px" ? this.style.marginLeft = "20px" : this.style.marginLeft = "400px";
+}
+
+
+function sies() {
+    alert("também te amo <3");  
+}
+
+
+document.getElementById("não").addEventListener("mouseover", displace) 
+
+
+document.getElementById("sim").addEventListener("click", sies)
+
+document.getElementById("sim").addEventListener("click", function(){
+    notify()
+});
+
+
+function notify(){
+    if (!("Notification" in window)) {
+        alert("S");
+    }else if(Notification.permission === "granted"){
+        var notification = new Notification("Hola mi amor :3"); 
+    }else if(Notification.permission !== "denied"){
+
+        Notification.requestPermission(function(permission) { // 
+
+            if(Notification.permission === "granted"){
+
+                var notification = new Notification("me ama?")
+            }
+        });
+    }
+}
